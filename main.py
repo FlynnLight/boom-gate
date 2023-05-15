4d6c66ad9d16751ec209e4a13026dae9acd4f5ff
FlynnLight <133617434+FlynnLight@users.noreply.github.com>
2023-05-15 12:10:37 +1000

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,5 @@
+Kitronik_ACCESSbit.barrier_control(Kitronik_ACCESSbit.BarrierPosition.UP)
+
+def on_forever():
+    pass
+basic.forever(on_forever)
---
+++ main.ts
@@ -0,0 +1,4 @@
+Kitronik_ACCESSbit.barrierControl(Kitronik_ACCESSbit.BarrierPosition.Up)
+basic.forever(function on_forever() {
+    
+})
